2663a9bd3dfbe4ffca58595fc7fbd4b047972a24
Hara Chung <harachung90@gmail.com>
2023-01-03 18:14:37 +1100

    Fifth commit. Added radio button and select option

--- src/App.js
+++ src/App.js
@@ -12,8 +12,11 @@ function App() {
             email: "",
             comments: "",
             isFriendly: true,
+            employment: "",
+            favColor: "",
         }
     )
+    console.log(formData.favColor)
 
     function handleChange(event) {
         const {name, value, type, checked} = event.target
@@ -62,6 +65,62 @@ function App() {
                 onChange={handleChange}
             />
             <label htmlFor="isFriendly">Are you friendly?</label>
+            <br/>
+            <br/>
+
+            <fieldset>
+                <legend>Current employment status</legend>
+
+                <input
+                    type="radio"
+                    id="unemployed"
+                    name="employment"
+                    value="unemployed"
+                    checked={formData.employment === "unemployed"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="unemployed">Unemployed</label>
+                <br />
+
+                <input
+                    type="radio"
+                    id="part-time"
+                    name="employment"
+                    value="part-time"
+                    checked={formData.employment === "part-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <br />
+
+                <input
+                    type="radio"
+                    id="full-time"
+                    name="employment"
+                    value="full-time"
+                    checked={formData.employment === "full-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="full-time">Full-time</label>
+                <br />
+
+                <label htmlFor="favColor">What is your favorite color?</label>
+                <br />
+                <select id="favColor"
+                    value={formData.favColor}
+                    onChange={handleChange}
+                    name="favColor"
+                >
+                    <option value="">-- Choose --</option>
+                    <option value="red">Red</option>
+                    <option value="orange">Orange</option>
+                    <option value="yellow">Yellow</option>
+                    <option value="green">Green</option>
+                    <option value="blue">Blue</option>
+                    <option value="indigo">Indigo</option>
+                    <option value="violet">Violet</option>
+                </select>
+            </fieldset>
         </form>
     )
 }
